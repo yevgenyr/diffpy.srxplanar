4d031df479054afe857dccc0a3bdcd5a1b0294b7
XiaohaoYang <sodestiny1@gmail.com>
2015-07-08 11:37:12 -0400

    fix import error on saveImage

--- diffpy/srxplanar/loadimage.py
+++ diffpy/srxplanar/loadimage.py
@@ -19,7 +19,7 @@ import os
 import fnmatch
 import sys
 from diffpy.srxplanar.srxplanarconfig import _configPropertyR
-import tifffile.imsave as saveImage
+from tifffile import imsave as saveImage
 
 try:
     import fabio
